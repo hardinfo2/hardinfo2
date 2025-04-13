70014ac20d061495d47db7d18eba6f41f13b86a8
hwspeedy <ns@bigbear.dk>
2025-04-13 12:32:55 +0200

    FIX Add temporary sysbench build+install script for epel10

---
+++ tools/build_and_install_sysbench_epel10.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+echo ""
+echo "This script builds+installs sysbench 1.0.20 for epel10 (Redhat,Oracle,Alme,Rocky...)"
+echo ""
+ARCH=$( uname -m )
+echo $ARCH;
+echo "needs additional packages to hardinfo2: libtool automake"
+yum install libtool automake
+
+#Latest release 1.0.20
+git clone https://github.com/akopytov/sysbench --branch 1.0
+
+#update luajit for riscv
+#cd sysbench/third_party/luajit
+#rm -rf luajit
+#git clone https://github.com/plctlab/LuaJIT/ luajit
+
+#update ck for riscv
+#cd ../concurrency_kit
+#rm -rf ck
+#git clone https://github.com/concurrencykit/ck ck
+#cd ../../
+
+#build
+./autogen.sh
+./configure --without-mysql
+make -j
+
+#only local install sysbench
+sudo cp src/sysbench /usr/local/bin/
+
+#cleanup
+cd ..
+rm -rf sysbench
+
+echo "Done - Sysbench is installed in /usr/local/bin"

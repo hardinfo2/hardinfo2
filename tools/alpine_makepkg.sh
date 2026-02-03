54cb72c6609e5d43ef4735c05bdfe184fa1b1a2f
hwspeedy <ns@bigbear.dk>
2026-02-03 20:16:14 +0100

    CPACK minor fix for multiarch pmos/alpine

--- tools/alpine_makepkg.sh
+++ tools/alpine_makepkg.sh
@@ -10,9 +10,6 @@ if [ $(id -u) -ne 0 ]; then
     abuild -r
 else
     abuild -r -F
-#    chown -R nobody ../hardinfo2
-#    sudo -u nobody abuild -r
-#    chown -R root ../hardinfo2
 fi
 
 #cleanup
@@ -20,7 +17,8 @@ rm -f APKBUILD
 #rm -f hardinfo2.install
 
 #rename and move result to build dir
-mv ~/packages/$USER/aarch64/*.apk ./build/
+ARCH=$(uname -m)
+mv ~/packages/$USER/$ARCH/hardinfo2*.apk ./build/
 cd build
 DISTRO=$(cat /etc/os-release |grep ^PRETTY_NAME=|awk '{sub(" ","-");sub("PRETTY_NAME=","");sub("\"","");sub("\"","")}1')
 rename r1 hardinfo2-${DISTRO} *.apk

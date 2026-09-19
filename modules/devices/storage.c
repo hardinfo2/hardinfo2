5e2d4cf1c6482f8241f613ce623a1af873c397d5
hwspeedy <ns@bigbear.dk>
2026-09-19 12:46:21 +0200

    FIX - storage handle serial null

--- modules/devices/storage.c
+++ modules/devices/storage.c
@@ -291,7 +291,7 @@ gboolean __scan_udisks2_devices(void) {
         size = size_human_readable((gfloat) disk->size);
         ven_tag = vendor_list_ribbon(ext->vendors, params.fmt_opts);
 
-        udisks2_storage_list = h_strdup_cprintf("$%s$%s=%s|%s|%s %s\n", udisks2_storage_list, devid, disk->block_dev, size, disk->serial, ven_tag ? ven_tag : "", disk->model);
+        udisks2_storage_list = h_strdup_cprintf("$%s$%s=%s|%s|%s %s\n", udisks2_storage_list, devid, disk->block_dev, size, (disk->serial ? disk->serial : ""), (ven_tag ? ven_tag : ""), disk->model);
         storage_icons = h_strdup_cprintf("Icon$%s$%s=%s.svg\n", storage_icons, devid, disk->model, icon);
         features = h_strdup_cprintf("%s", features, disk->removable ? _("Removable"): _("Fixed"));
 
